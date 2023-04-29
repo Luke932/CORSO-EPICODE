21dca9f4af6b0549137b7cf7ead4a9ffd4de7f06
Luke932 <126018318+Luke932@users.noreply.github.com>
2023-04-30 01:00:09 +0200

    progetto

--- Corso/ProgettiSettimanali/Mese.N.02/ProgettoSettimanaleN.01/progetto/assets/js/script.js
+++ Corso/ProgettiSettimanali/Mese.N.02/ProgettoSettimanaleN.01/progetto/assets/js/script.js
@@ -1,31 +1,16 @@
-const scrollElement = document.getElementById('stRd');
-const containerNav = document.getElementById('navBar');
-const getStarted = document.getElementById('greenButton');
-let emme = document.querySelectorAll("g[stroke-linecap='butt']");
-
-setInterval(() => {
-    let random = Math.round(Math.random() * emme.length - 1);
-    emme[random].classList.toggle('invisible');
-}, 100)
-
-window.addEventListener('scroll', () => {
-
-    if(window.scrollY >= 400){
-
-        containerNav.classList.remove('baseContainerNav');
-        containerNav.classList.add('whiteContainerNav');
-
-        getStarted.classList.remove('blackBtn');
-        getStarted.classList.add('greenBtn'); 
-
-    }else if(window.scrollY < 400){
-
-        containerNav.classList.add('baseContainerNav');
-        containerNav.classList.remove('whiteContainerNav');
-
-        getStarted.classList.add('blackBtn');
-        getStarted.classList.remove('greenBtn');
-
-    }
-    
+const change = 490;
+const modBtn = document.getElementById("btnG");
+const headCh = document.getElementById("head");
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= change) {
+    modBtn.classList.add("btnG");
+    headCh.classList.add("headCh");
+    modBtn.classList.remove("outBtn");
+    headCh.classList.remove("outHead");
+  } else if (window.screenY < change) {
+    modBtn.classList.remove("btnG");
+    headCh.classList.remove("headCh");
+    modBtn.classList.add("outBtn");
+    headCh.classList.add("outHead");
+  }
 });
