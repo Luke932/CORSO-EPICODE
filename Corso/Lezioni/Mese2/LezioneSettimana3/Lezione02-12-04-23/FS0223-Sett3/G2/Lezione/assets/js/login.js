164e7edae8952dc79dcf8f4db8ebb463a18d0f95
Luke932 <126018318+Luke932@users.noreply.github.com>
2023-05-09 17:02:55 +0200

    esercizio giorno 09/05/23

--- Corso/Lezioni/Mese2/LezioneSettimana3/Lezione02-12-04-23/FS0223-Sett3/G2/Lezione/assets/js/login.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('entra').addEventListener('click', function() {
-    let utente = document.getElementById('utente').value;
-    if (utente == '') { // Se non c'è il nome utente blocca la procedura
-        alert('Inserire il nome utente!');
-        return;
-    }
-    localStorage.setItem('utente', utente); // Scrive nel localStorage il nome utente per passarlo alla pagina successiva e verificare che l'utente sia autenticato
-    location.href = '../../todo.html'; // passa alla pagina todo.html
-});
