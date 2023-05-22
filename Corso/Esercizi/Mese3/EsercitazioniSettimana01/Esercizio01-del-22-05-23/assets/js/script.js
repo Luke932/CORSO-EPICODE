fc533bf61c92d05d7fcc5e62d1c399c2c9bd3fc5
Luke932 <126018318+Luke932@users.noreply.github.com>
2023-05-22 17:12:58 +0200

    Esercizio typescript 1

--- Corso/Esercizi/Mese3/EsercitazioniSettimana01/Esercizio01-del-22-05-23/assets/js/script.js
+++ Corso/Esercizi/Mese3/EsercitazioniSettimana01/Esercizio01-del-22-05-23/assets/js/script.js
@@ -1,17 +1,17 @@
-var btnVerifica = document.getElementById('verifica');
-var btnReset = document.getElementById('reset');
-var giocatore1 = document.getElementById('giocatore1');
-var giocatore2 = document.getElementById('giocatore2');
+var btnVerifica = document.getElementById("verifica");
+var btnReset = document.getElementById("reset");
+var giocatore1 = document.getElementById("giocatore1");
+var giocatore2 = document.getElementById("giocatore2");
 var valore1;
 var valore2;
-var messaggio = document.getElementById('risultato');
+var messaggio = document.getElementById("risultato");
 var estratto;
-window.addEventListener('load', function () {
-    btnReset.setAttribute('disabled', 'true');
-    btnReset.style.backgroundColor = 'red';
-    btnReset.style.color = 'white';
+window.addEventListener("load", function () {
+    btnReset.setAttribute("disabled", "true");
+    btnReset.style.backgroundColor = "red";
+    btnReset.style.color = "white";
 });
-btnVerifica.addEventListener('click', function () {
+btnVerifica.addEventListener("click", function () {
     valore1 = Number(giocatore1.value);
     valore2 = Number(giocatore2.value);
     verifica();
@@ -20,20 +20,20 @@ var verifica = function () {
     if (!valore1 || !valore2) {
         if (messaggio !== null) {
             messaggio.innerHTML =
-                'Attenzione! Entrambi i giocatori devono inserire un valore!';
+                "Attenzione! Entrambi i giocatori devono inserire un valore!";
         }
         return;
     }
     else if (valore1 < 1 || valore2 < 1 || valore1 > 100 || valore2 > 100) {
         if (messaggio !== null) {
             messaggio.innerHTML =
-                'Attenzione! Inserire un valore numerico tra 1 e 100!';
+                "Attenzione! Inserire un valore numerico tra 1 e 100!";
         }
         return;
     }
     else if (valore1 === valore2) {
         if (messaggio !== null) {
-            messaggio.innerHTML = 'Attenzione! Inserire valori diversi!';
+            messaggio.innerHTML = "Attenzione! Inserire valori diversi!";
         }
         return;
     }
@@ -46,66 +46,66 @@ var estrai = function () {
     estratto = Math.floor(Math.random() * 100 + 1);
 };
 var calcola = function () {
-    btnReset.removeAttribute('disabled');
-    btnReset.style.backgroundColor = 'green';
-    giocatore1.setAttribute('disabled', 'true');
-    giocatore1.style.backgroundColor = 'lightgray';
-    giocatore2.setAttribute('disabled', 'true');
-    giocatore2.style.backgroundColor = 'lightgray';
+    btnReset.removeAttribute("disabled");
+    btnReset.style.backgroundColor = "green";
+    giocatore1.setAttribute("disabled", "true");
+    giocatore1.style.backgroundColor = "lightgray";
+    giocatore2.setAttribute("disabled", "true");
+    giocatore2.style.backgroundColor = "lightgray";
     if (messaggio !== null) {
-        document.getElementById('estratto').innerHTML = "Numero estratto: ".concat(estratto);
+        document.getElementById("estratto").innerHTML = "Numero estratto: ".concat(estratto);
     }
     switch (true) {
         case valore1 === estratto:
             if (messaggio !== null) {
-                messaggio.innerHTML = 'Il giocatore 1 ha indovinato!';
+                messaggio.innerHTML = "Il giocatore 1 ha indovinato!";
             }
             break;
         case valore2 === estratto:
             if (messaggio !== null) {
-                messaggio.innerHTML = 'Il giocatore 2 ha indovinato!';
+                messaggio.innerHTML = "Il giocatore 2 ha indovinato!";
             }
             break;
         case Math.abs(estratto - valore1) < Math.abs(estratto - valore2):
             if (messaggio !== null) {
                 messaggio.innerHTML =
-                    'Nessuno ha vinto ma il giocatore 1 si è avvicinato di più';
+                    "Nessuno ha vinto ma il giocatore 1 si è avvicinato di più";
             }
             break;
         case Math.abs(estratto - valore2) < Math.abs(estratto - valore1):
             if (messaggio !== null) {
                 messaggio.innerHTML =
-                    'Nessuno ha vinto ma il giocatore 2 si è avvicinato di più';
+                    "Nessuno ha vinto ma il giocatore 2 si è avvicinato di più";
             }
             break;
         default:
             if (messaggio !== null) {
-                messaggio.innerHTML = 'Pareggio!';
+                messaggio.innerHTML = "Pareggio!";
             }
             break;
     }
-    btnVerifica.setAttribute('disabled', 'true');
-    btnVerifica.style.backgroundColor = 'red';
-    btnVerifica.style.color = 'white';
+    btnVerifica.setAttribute("disabled", "true");
+    btnVerifica.style.backgroundColor = "red";
+    btnVerifica.style.color = "white";
 };
-btnReset.addEventListener('click', function () {
-    btnVerifica.removeAttribute('disabled');
-    btnVerifica.removeAttribute('style');
-    giocatore1.removeAttribute('disabled');
-    giocatore1.removeAttribute('style');
-    giocatore2.removeAttribute('disabled');
-    giocatore2.removeAttribute('style');
-    giocatore1.value = '';
-    giocatore2.value = '';
+btnReset.addEventListener("click", function () {
+    btnVerifica.removeAttribute("disabled");
+    btnVerifica.removeAttribute("style");
+    giocatore1.removeAttribute("disabled");
+    giocatore1.removeAttribute("style");
+    giocatore2.removeAttribute("disabled");
+    giocatore2.removeAttribute("style");
+    giocatore1.value = "";
+    giocatore2.value = "";
     if (messaggio !== null) {
-        messaggio.innerHTML = '';
+        messaggio.innerHTML = "";
     }
-    if (document.getElementById('estratto') !== null) {
-        document.getElementById('estratto').innerHTML = '';
+    if (document.getElementById("estratto") !== null) {
+        document.getElementById("estratto").innerHTML = "";
     }
-    btnReset.setAttribute('disabled', 'true');
-    btnReset.style.backgroundColor = 'red';
-    btnReset.style.color = 'white';
+    btnReset.setAttribute("disabled", "true");
+    btnReset.style.backgroundColor = "red";
+    btnReset.style.color = "white";
 });
 var numberRandom = Math.floor(Math.random() * 100 + 1);
 console.log(numberRandom);
@@ -130,3 +130,42 @@ else if (Math.abs(numberRandom - player2) < Math.abs(numberRandom - player1)) {
 }
 else
     console.log('Pareggio');
+//============================================================================================
+var random = function () {
+    return Math.floor(Math.random() * 100 + 1);
+};
+var Numbers;
+(function (Numbers) {
+    Numbers[Numbers["player1"] = random()] = "player1";
+    Numbers[Numbers["player2"] = random()] = "player2";
+    Numbers[Numbers["numberRandom"] = random()] = "numberRandom";
+})(Numbers || (Numbers = {}));
+var play = function () {
+    console.log("Player 1: ".concat(Numbers.player1));
+    console.log("Player 2: ".concat(Numbers.player2));
+    console.log("Number random: ".concat(Numbers.numberRandom));
+    switch (true) {
+        case Number(Numbers.player1) === Numbers.numberRandom:
+            console.log("Il player 1 ha indovinato");
+            break;
+        case Number(Numbers.player2) === Numbers.numberRandom:
+            console.log("Il player 2 ha indovinato");
+            break;
+        case Number(Numbers.player1) === Numbers.numberRandom &&
+            Number(Numbers.player2) === Numbers.numberRandom:
+            console.log("Entrambi i giocatori hanno indovinato");
+            break;
+        case Math.abs(Numbers.numberRandom - Numbers.player1) <
+            Math.abs(Numbers.numberRandom - Numbers.player2):
+            console.log("Nessuno ha vinto ma il player 1 si è avvicinato di più");
+            break;
+        case Math.abs(Numbers.numberRandom - Numbers.player2) <
+            Math.abs(Numbers.numberRandom - Numbers.player1):
+            console.log("Nessuno ha vinto ma il player 2 si è avvicinato di più");
+            break;
+        default:
+            console.log("Pareggio");
+            break;
+    }
+};
+play();
--- Corso/Esercizi/Mese3/EsercitazioniSettimana01/Esercizio01-del-22-05-23/assets/ts/script.ts
+++ Corso/Esercizi/Mese3/EsercitazioniSettimana01/Esercizio01-del-22-05-23/assets/ts/script.ts
@@ -1,124 +1,126 @@
-const btnVerifica = document.getElementById('verifica') as HTMLButtonElement;
-const btnReset = document.getElementById('reset') as HTMLButtonElement;
-const giocatore1 = document.getElementById('giocatore1') as HTMLInputElement;
-const giocatore2 = document.getElementById('giocatore2') as HTMLInputElement;
+const btnVerifica = document.getElementById("verifica") as HTMLButtonElement;
+const btnReset = document.getElementById("reset") as HTMLButtonElement;
+const giocatore1 = document.getElementById("giocatore1") as HTMLInputElement;
+const giocatore2 = document.getElementById("giocatore2") as HTMLInputElement;
 let valore1: number;
 let valore2: number;
-const messaggio = document.getElementById('risultato');
+const messaggio = document.getElementById("risultato");
 let estratto: number;
 
-window.addEventListener('load', function () {
-    btnReset.setAttribute('disabled', 'true');
-    btnReset.style.backgroundColor = 'red';
-    btnReset.style.color = 'white';
+window.addEventListener("load", function () {
+  btnReset.setAttribute("disabled", "true");
+  btnReset.style.backgroundColor = "red";
+  btnReset.style.color = "white";
 });
 
-btnVerifica.addEventListener('click', function () {
-    valore1 = Number(giocatore1.value);
-    valore2 = Number(giocatore2.value);
-    verifica();
+btnVerifica.addEventListener("click", function () {
+  valore1 = Number(giocatore1.value);
+  valore2 = Number(giocatore2.value);
+  verifica();
 });
 
 const verifica = () => {
-    if (!valore1 || !valore2) {
-        if (messaggio !== null) {
-            messaggio.innerHTML =
-                'Attenzione! Entrambi i giocatori devono inserire un valore!';
-        }
-        return;
-    } else if (valore1 < 1 || valore2 < 1 || valore1 > 100 || valore2 > 100) {
-        if (messaggio !== null) {
-            messaggio.innerHTML =
-                'Attenzione! Inserire un valore numerico tra 1 e 100!';
-        }
-        return;
-    } else if (valore1 === valore2) {
-        if (messaggio !== null) {
-            messaggio.innerHTML = 'Attenzione! Inserire valori diversi!';
-        }
-        return;
-    } else {
-        estrai();
-        calcola();
+  if (!valore1 || !valore2) {
+    if (messaggio !== null) {
+      messaggio.innerHTML =
+        "Attenzione! Entrambi i giocatori devono inserire un valore!";
     }
+    return;
+  } else if (valore1 < 1 || valore2 < 1 || valore1 > 100 || valore2 > 100) {
+    if (messaggio !== null) {
+      messaggio.innerHTML =
+        "Attenzione! Inserire un valore numerico tra 1 e 100!";
+    }
+    return;
+  } else if (valore1 === valore2) {
+    if (messaggio !== null) {
+      messaggio.innerHTML = "Attenzione! Inserire valori diversi!";
+    }
+    return;
+  } else {
+    estrai();
+    calcola();
+  }
 };
 
 const estrai = () => {
-    estratto = Math.floor(Math.random() * 100 + 1);
+  estratto = Math.floor(Math.random() * 100 + 1);
 };
 
 const calcola = () => {
-    btnReset.removeAttribute('disabled');
-    btnReset.style.backgroundColor = 'green';
-    giocatore1.setAttribute('disabled', 'true');
-    giocatore1.style.backgroundColor = 'lightgray';
-    giocatore2.setAttribute('disabled', 'true');
-    giocatore2.style.backgroundColor = 'lightgray';
+  btnReset.removeAttribute("disabled");
+  btnReset.style.backgroundColor = "green";
+  giocatore1.setAttribute("disabled", "true");
+  giocatore1.style.backgroundColor = "lightgray";
+  giocatore2.setAttribute("disabled", "true");
+  giocatore2.style.backgroundColor = "lightgray";
 
-    if (messaggio !== null) {
-        document.getElementById('estratto')!.innerHTML = `Numero estratto: ${estratto}`;
-    }
+  if (messaggio !== null) {
+    document.getElementById(
+      "estratto"
+    )!.innerHTML = `Numero estratto: ${estratto}`;
+  }
 
-    switch (true) {
-        case valore1 === estratto:
-            if (messaggio !== null) {
-                messaggio.innerHTML = 'Il giocatore 1 ha indovinato!';
-            }
-            break;
+  switch (true) {
+    case valore1 === estratto:
+      if (messaggio !== null) {
+        messaggio.innerHTML = "Il giocatore 1 ha indovinato!";
+      }
+      break;
 
-        case valore2 === estratto:
-            if (messaggio !== null) {
-                messaggio.innerHTML = 'Il giocatore 2 ha indovinato!';
-            }
-            break;
+    case valore2 === estratto:
+      if (messaggio !== null) {
+        messaggio.innerHTML = "Il giocatore 2 ha indovinato!";
+      }
+      break;
 
-        case Math.abs(estratto - valore1) < Math.abs(estratto - valore2):
-            if (messaggio !== null) {
-                messaggio.innerHTML =
-                    'Nessuno ha vinto ma il giocatore 1 si è avvicinato di più';
-            }
-            break;
+    case Math.abs(estratto - valore1) < Math.abs(estratto - valore2):
+      if (messaggio !== null) {
+        messaggio.innerHTML =
+          "Nessuno ha vinto ma il giocatore 1 si è avvicinato di più";
+      }
+      break;
 
-        case Math.abs(estratto - valore2) < Math.abs(estratto - valore1):
-            if (messaggio !== null) {
-                messaggio.innerHTML =
-                    'Nessuno ha vinto ma il giocatore 2 si è avvicinato di più';
-            }
-            break;
+    case Math.abs(estratto - valore2) < Math.abs(estratto - valore1):
+      if (messaggio !== null) {
+        messaggio.innerHTML =
+          "Nessuno ha vinto ma il giocatore 2 si è avvicinato di più";
+      }
+      break;
 
-        default:
-            if (messaggio !== null) {
-                messaggio.innerHTML = 'Pareggio!';
-            }
-            break;
-    }
+    default:
+      if (messaggio !== null) {
+        messaggio.innerHTML = "Pareggio!";
+      }
+      break;
+  }
 
-    btnVerifica.setAttribute('disabled', 'true');
-    btnVerifica.style.backgroundColor = 'red';
-    btnVerifica.style.color = 'white';
+  btnVerifica.setAttribute("disabled", "true");
+  btnVerifica.style.backgroundColor = "red";
+  btnVerifica.style.color = "white";
 };
 
-btnReset.addEventListener('click', function () {
-    btnVerifica.removeAttribute('disabled');
-    btnVerifica.removeAttribute('style');
-    giocatore1.removeAttribute('disabled');
-    giocatore1.removeAttribute('style');
-    giocatore2.removeAttribute('disabled');
-    giocatore2.removeAttribute('style');
-    giocatore1.value = '';
-    giocatore2.value = '';
-    if (messaggio !== null) {
-        messaggio.innerHTML = '';
-    }
-    if (document.getElementById('estratto') !== null) {
-        document.getElementById('estratto')!.innerHTML = '';
-    }
-    btnReset.setAttribute('disabled', 'true');
-    btnReset.style.backgroundColor = 'red';
-    btnReset.style.color = 'white';
+btnReset.addEventListener("click", function () {
+  btnVerifica.removeAttribute("disabled");
+  btnVerifica.removeAttribute("style");
+  giocatore1.removeAttribute("disabled");
+  giocatore1.removeAttribute("style");
+  giocatore2.removeAttribute("disabled");
+  giocatore2.removeAttribute("style");
+  giocatore1.value = "";
+  giocatore2.value = "";
+  if (messaggio !== null) {
+    messaggio.innerHTML = "";
+  }
+  if (document.getElementById("estratto") !== null) {
+    document.getElementById("estratto")!.innerHTML = "";
+  }
+  btnReset.setAttribute("disabled", "true");
+  btnReset.style.backgroundColor = "red";
+  btnReset.style.color = "white";
 });
 
-  const numberRandom: number = Math.floor(Math.random() * 100 + 1);
+const numberRandom: number = Math.floor(Math.random() * 100 + 1);
   console.log(numberRandom);
   
   const player1: number = Math.floor(Math.random() * 100 + 1);
@@ -138,6 +140,53 @@ btnReset.addEventListener('click', function () {
   }else if (Math.abs(numberRandom - player2) < Math.abs(numberRandom - player1)) {
     console.log('Nessuno ha vinto ma il player 2 si è avvicinato di più');
   }else console.log('Pareggio');
-  
-            
 
+
+  //============================================================================================
+
+const random = (): number => {
+  return Math.floor(Math.random() * 100 + 1);
+};
+
+enum Numbers {
+  player1 = random(),
+  player2 = random(),
+  numberRandom = random(),
+}
+
+const play = (): void => {
+  console.log(`Player 1: ${Numbers.player1}`);
+  console.log(`Player 2: ${Numbers.player2}`);
+  console.log(`Number random: ${Numbers.numberRandom}`);
+
+  switch (true) {
+    case Number(Numbers.player1) === Numbers.numberRandom:
+      console.log("Il player 1 ha indovinato");
+      break;
+
+    case Number(Numbers.player2) === Numbers.numberRandom:
+      console.log("Il player 2 ha indovinato");
+      break;
+
+    case Number(Numbers.player1) === Numbers.numberRandom &&
+      Number(Numbers.player2) === Numbers.numberRandom:
+      console.log("Entrambi i giocatori hanno indovinato");
+      break;
+
+    case Math.abs(Numbers.numberRandom - Numbers.player1) <
+      Math.abs(Numbers.numberRandom - Numbers.player2):
+      console.log("Nessuno ha vinto ma il player 1 si è avvicinato di più");
+      break;
+
+    case Math.abs(Numbers.numberRandom - Numbers.player2) <
+      Math.abs(Numbers.numberRandom - Numbers.player1):
+      console.log("Nessuno ha vinto ma il player 2 si è avvicinato di più");
+      break;
+
+    default:
+      console.log("Pareggio");
+      break;
+  }
+};
+
+play();
