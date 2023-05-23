74fa5f61597fe7f8212c16054ae2dc7d5bf4bae9
Luke932 <126018318+Luke932@users.noreply.github.com>
2023-05-23 21:40:42 +0200

    esercizio giorno 2 con dom

--- Corso/Esercizi/Mese3/EsercitazioniSettimana01/Esercizio02-del-23-05-23/assets/js/dom.js
+++ Corso/Esercizi/Mese3/EsercitazioniSettimana01/Esercizio02-del-23-05-23/assets/js/dom.js
@@ -1,3 +1,69 @@
 "use strict";
 const conto1 = document.getElementById('conto1');
 const primoConto = document.getElementById('primoConto');
+const btnBalance = document.getElementById('balanceInitial');
+const form = document.getElementById('topolino');
+const primoVersamento = document.getElementById('primoVersamento');
+const btnVers = document.getElementById('vers');
+const result = document.getElementById('result');
+const primoPrelievo = document.getElementById('primoPrelievo');
+const btnPrel = document.getElementById('prel');
+const result2 = document.getElementById('result2');
+const secondoVersamento = document.getElementById('secondoVersamento');
+const btnVers2 = document.getElementById('vers2');
+const result3 = document.getElementById('result3');
+const secondoPrelievo = document.getElementById('secondoPrelievo');
+const btnPrel2 = document.getElementById('prel2');
+const result4 = document.getElementById('result4');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let valore1 = Number(primoConto === null || primoConto === void 0 ? void 0 : primoConto.value);
+    let valore2 = Number(primoVersamento === null || primoVersamento === void 0 ? void 0 : primoVersamento.value);
+    let valore3 = Number(primoPrelievo === null || primoPrelievo === void 0 ? void 0 : primoPrelievo.value);
+    let valore4 = Number(secondoVersamento === null || secondoVersamento === void 0 ? void 0 : secondoVersamento.value);
+    let valore5 = Number(secondoPrelievo === null || secondoPrelievo === void 0 ? void 0 : secondoPrelievo.value);
+    class Money {
+        constructor(_valore1, _valore2, _valore3, _valore4, _valore5) {
+            this.valore1 = _valore1;
+            this.valore2 = _valore2;
+            this.valore3 = _valore3;
+            this.valore4 = _valore4;
+            this.valore5 = _valore5;
+        }
+        getConto1(saldoIniziale) {
+            return saldoIniziale;
+        }
+        setOneDeposit(saldoIniziale, versamento) {
+            saldoIniziale += versamento;
+            return saldoIniziale;
+        }
+        setOneWithDraw(saldoIniziale, prelievo) {
+            saldoIniziale -= prelievo;
+            return saldoIniziale;
+        }
+        setTwoDeposit(saldoIniziale, versamento) {
+            saldoIniziale += versamento;
+            return saldoIniziale;
+        }
+        setTwoWithDraw(saldoIniziale, prelievo) {
+            saldoIniziale -= prelievo;
+            return saldoIniziale;
+        }
+    }
+    let newMoney = new Money(valore1, valore2, valore3, valore4, valore5);
+    if (conto1 !== null) {
+        conto1.innerHTML = newMoney.getConto1(valore1);
+    }
+    if (result !== null) {
+        result.innerHTML = newMoney.setOneDeposit(valore1, valore2);
+    }
+    if (result2 !== null) {
+        result2.innerHTML = newMoney.setOneWithDraw(valore1 + valore2, valore3);
+    }
+    if (result3 !== null) {
+        result3.innerHTML = newMoney.setTwoDeposit(valore1 + valore2 - valore3, valore4);
+    }
+    if (result4 !== null) {
+        result4.innerHTML = newMoney.setTwoWithDraw(valore1 + valore2 - valore3 + valore4, valore5);
+    }
+});
--- Corso/Esercizi/Mese3/EsercitazioniSettimana01/Esercizio02-del-23-05-23/assets/ts/dom.ts
+++ Corso/Esercizi/Mese3/EsercitazioniSettimana01/Esercizio02-del-23-05-23/assets/ts/dom.ts
@@ -1,3 +1,97 @@
 const conto1 = document.getElementById('conto1') as HTMLElement | null;
-const primoConto = document.getElementById ('primoConto') as HTMLElement | null;
+const primoConto = document.getElementById ('primoConto') as HTMLInputElement | null;
+const btnBalance = document.getElementById ('balanceInitial') as HTMLButtonElement;
+const form = document.getElementById ('topolino') as HTMLFormElement;
 
+
+const primoVersamento = document.getElementById ('primoVersamento') as HTMLInputElement | null;
+const btnVers = document.getElementById ('vers') as HTMLButtonElement;
+const result = document.getElementById ('result') as HTMLElement | null;
+
+const primoPrelievo = document.getElementById ('primoPrelievo') as HTMLInputElement | null;
+const btnPrel = document.getElementById ('prel') as HTMLButtonElement;
+const result2 = document.getElementById ('result2') as HTMLElement | null;
+
+const secondoVersamento = document.getElementById ('secondoVersamento') as HTMLInputElement | null;
+const btnVers2 = document.getElementById ('vers2') as HTMLButtonElement;
+const result3 = document.getElementById ('result3') as HTMLElement | null;
+
+const secondoPrelievo = document.getElementById ('secondoPrelievo') as HTMLInputElement | null;
+const btnPrel2 = document.getElementById ('prel2') as HTMLButtonElement;
+const result4 = document.getElementById ('result4') as HTMLElement | null;
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+
+let valore1 = Number(primoConto?.value);
+let valore2 = Number(primoVersamento?.value);
+let valore3 = Number(primoPrelievo?.value);
+let valore4 = Number(secondoVersamento?.value);
+let valore5 = Number(secondoPrelievo?.value);
+
+class Money {
+    valore1: number;
+    valore2: number;
+    valore3: number;
+    valore4: number;
+    valore5: number;
+    constructor ( _valore1: number,_valore2: number, _valore3: number, _valore4: number, _valore5: number){
+        this.valore1 = _valore1;
+        this.valore2 = _valore2;
+        this.valore3 = _valore3;
+        this.valore4 = _valore4;
+        this.valore5 = _valore5;
+    }
+    
+    getConto1(saldoIniziale:number) : any {
+        return saldoIniziale;
+    }
+
+    setOneDeposit (saldoIniziale:number, versamento:number) : any {
+        saldoIniziale += versamento;
+        return saldoIniziale;
+    }
+
+    setOneWithDraw (saldoIniziale:number, prelievo:number) : any {
+        saldoIniziale -= prelievo;
+        return saldoIniziale;
+    }
+
+    setTwoDeposit (saldoIniziale:number,versamento:number) : any {
+        saldoIniziale += versamento;
+        return saldoIniziale;
+    }
+
+    setTwoWithDraw (saldoIniziale:number,prelievo:number) : any {
+        saldoIniziale -= prelievo;
+        return saldoIniziale;
+    }
+}
+
+let newMoney = new Money(valore1, valore2, valore3, valore4, valore5);
+
+
+
+
+if (conto1 !== null) {
+    conto1.innerHTML = newMoney.getConto1(valore1);
+}
+if (result !== null) {
+    result.innerHTML = newMoney.setOneDeposit(valore1 , valore2);
+}
+
+if (result2 !== null) {
+    result2.innerHTML = newMoney.setOneWithDraw(valore1 + valore2,valore3);
+}
+
+if (result3 !== null) {
+    result3.innerHTML = newMoney.setTwoDeposit(valore1 + valore2 - valore3, valore4 );
+}
+
+if (result4 !== null) {
+    result4.innerHTML = newMoney.setTwoWithDraw(valore1 + valore2 - valore3 + valore4, valore5);
+}
+
+})
